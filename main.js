e45dc8fd3ada788cfd04d6f51e7e799a63ff74b3
Mick Negenman <36791@ma-web.nl>
2023-10-16 14:51:19 +0200

    Basis code opgebouwd

--- main.js
+++
@@ -1,2 +0,0 @@
-//const readline = require("readline-sync");
-console.log("Hello World!");
---
+++ src/main.js
@@ -0,0 +1,47 @@
+// Packages
+const readline = require("readline-sync");
+
+// Flowchart
+const flowChart = [
+    {
+        decision: "Decision 1",
+        options: [
+            { option: "Option 1", points_to: 2 },
+            { option: "Option 2", points_to: 1 }
+        ]
+    },
+    {
+        decision: "Decision 2",
+        options: [
+            { option: "Option 1", points_to: 0 },
+            { option: "Option 2", points_to: 2 }
+        ]
+    },
+    {
+        decision: "Decision 3",
+        options: [
+            { option: "Option 1", points_to: 0 },
+            { option: "Option 2", points_to: -1 }
+        ]
+    }
+];
+
+const endings = [
+    "Gefeliciteerd, je bent bij het goede einde terecht gekomen",
+    "Helaas, je bent bij het slechte einde terecht gekomen",
+    "Je bent bij het normale einde terecht gekomen"
+];
+
+// Main
+let index = 0;
+
+do {
+    const { decision, options } = flowChart[index];
+    const option = readline.keyInSelect(options.map(v => v.option), decision, { cancel: false });
+
+    index = options[option].points_to;
+
+    console.log('\n' + '='.repeat(25));
+} while (index >= 0)
+
+console.log('\n' + endings[Math.abs(index) - 1]);
