e619d849018dfd4f52f0c1c90ef84e36ddf81216
Mick Negenman <36791@ma-web.nl>
2023-10-16 21:13:09 +0200

    Comments toegevoegd

--- src/main.js
+++ src/main.js
@@ -8,17 +8,20 @@ let index = 0;
 while (true) {
     const { decision, options } = flowChart.at(index);
 
+    // Check if you've reached an end of the flowchart
     if (!options || options.length === 0) {
         console.log('\n' + decision);
         break;
     }
 
+    // Get the options and prompt them
     const optionIndex = readline.keyInSelect(
         options.map(v => v.option),
         decision,
         { cancel: false }
     );
 
+    // Set the index to the one the option points to and print a separator
     index = options[optionIndex].pointsTo;
     console.log('\n' + '='.repeat(25));
 }
