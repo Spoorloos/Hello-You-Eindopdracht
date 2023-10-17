e12ef8100b8632ca7ca518a5dd263c671bc201c2
Mick Negenman <36791@ma-web.nl>
2023-10-17 09:18:58 +0200

    Optie om het spel te herstarten toegevoegd

--- src/main.js
+++ src/main.js
@@ -1,31 +1,33 @@
 import readline from "readline-sync";
 import { flowChart } from "./flowchart.js";
 
-// Navigate flowchart
-let index = 0;
+do {
+    // Navigate flowchart
+    let index = 0;
 
-while (true) {
-    const { decision, art, options } = flowChart.at(index);
+    while (true) {
+        const { decision, art, options } = flowChart.at(index);
 
-    // Check if you've reached an end of the flowchart
-    if (!options || options.length === 0) {
-        console.log('\n' + decision);
-        break;
+        // Check if you've reached an end of the flowchart
+        if (!options || options.length === 0) {
+            console.log('\n' + decision);
+            break;
+        }
+
+        // If there's ascii art, print it
+        if (art) {
+            console.log('\n' + art);
+        }
+
+        // Get the options and prompt them
+        const optionIndex = readline.keyInSelect(
+            options.map(v => v.option),
+            decision,
+            { cancel: false }
+        );
+
+        // Set the index to the one the option points to and print a separator
+        index = options[optionIndex].pointsTo;
+        console.log('\n' + '='.repeat(25));
     }
-
-    // If there's ascii art, print it
-    if (art) {
-        console.log('\n' + art);
-    }
-
-    // Get the options and prompt them
-    const optionIndex = readline.keyInSelect(
-        options.map(v => v.option),
-        decision,
-        { cancel: false }
-    );
-
-    // Set the index to the one the option points to and print a separator
-    index = options[optionIndex].pointsTo;
-    console.log('\n' + '='.repeat(25));
-}
+} while (readline.keyInYN("Wil je het spel herstarten?"))
